ad8d05f9d37029607750eeead4d496684a26bcd9
5iftyspam <5iftyspam@gmail.com>
2022-01-19 18:59:44 -0500

    made and referenced js in html to an actual file

---
+++ static/styles/enter.js
@@ -0,0 +1,8 @@
+var input = document.getElementById("cmdinput");
+        input.addEventListener("keyup", function(event) {
+             // Number 13 is the "Enter" key on the keyboard
+            if (event.keyCode === 13) {
+                form.submit();
+    
+  }
+});

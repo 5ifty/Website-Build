c911b72efb7bc43353f040bf2438fd675b06fa9c
Harry <5iftyy666@gmail.com>
2022-02-20 00:23:16 +0000

    Added type writer effect and JS to run it when button is clicked, rather than it run when the page is ready, partially  removed firebase SDK as it cant deploy python based apps.... & General clean ups

--- static/styles/onclick.js
+++ static/styles/onclick.js
@@ -1,7 +1,12 @@
+
 function unhide() {
     var hid = document.getElementsByClassName("hidden");
     // Emulates jQuery $(element).is(':hidden');
     if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0) {
         hid[0].style.visibility = "visible";
     }
+}
+
+function typewrite () {
+  document.getElementById('typeabout').classList.add("typewriter-text");
 }

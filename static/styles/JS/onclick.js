93cda928155187c545d77462a92608ee6771ff49
Harry <5iftyy666@gmail.com>
2022-03-19 16:31:49 +0000

    Tiles and cards(unstable) and Better containers

--- static/styles/JS/onclick.js
+++ static/styles/JS/onclick.js
@@ -14,8 +14,8 @@ function typewrite () {
 
 
 $(function() {
-  $('a[href*=#]').on('click', function(e) {
-    e.preventDefault();
-    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
-  });
-});
+   $('.scroll-down').click (function() {
+     $('html, body').animate({scrollTop: $('section.ok').offset().top }, 'slow');
+     return false;
+   });
+ });
